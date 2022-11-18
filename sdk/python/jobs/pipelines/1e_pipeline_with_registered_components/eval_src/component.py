7eedfdc455a1a67db6f89e032dda7fd95e0f7f3f
Ziqi Wang <zikeiwong@outlook.com>
2022-11-18 10:56:13 +0800

    Ramp up Tests

--- sdk/python/jobs/pipelines/1e_pipeline_with_registered_components/eval_src/component.py
+++ sdk/python/jobs/pipelines/1e_pipeline_with_registered_components/eval_src/component.py
@@ -1,20 +1,23 @@
+
 from mldesigner import command_component, Input, Output
 
-
-@command_component(display_name="Eval Model", version="0.0.9")
+@command_component(
+    display_name="Eval Model",
+    version="0.0.9",
+)
 def eval_model(
-    scoring_result: Input(type="uri_folder"), eval_output: Output(type="uri_folder")
+    scoring_result: Input(type="uri_folder"),
+    eval_output: Output(type="uri_folder"),
 ):
     """A dummy eval component defined by dsl component."""
 
     from pathlib import Path
     from datetime import datetime
-
-    print("hello evaluation world...")
+    print ("hello evaluation world...")
 
     lines = [
-        f"Scoring result path: {scoring_result}",
-        f"Evaluation output path: {eval_output}",
+        f'Scoring result path: {scoring_result}',
+        f'Evaluation output path: {eval_output}',
     ]
 
     for line in lines:
@@ -24,4 +27,4 @@ def eval_model(
     # Here only output a dummy file for demo.
     curtime = datetime.now().strftime("%b-%d-%Y %H:%M:%S")
     eval_msg = f"Eval done at {curtime}\n"
-    (Path(eval_output) / "eval_result.txt").write_text(eval_msg)
+    (Path(eval_output) / 'eval_result.txt').write_text(eval_msg)
